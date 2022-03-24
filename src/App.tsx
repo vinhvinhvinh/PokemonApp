934d0aab79b535c257ceb1537cdcfe271ef57b41
Vinh <ntvinh0606@gmail.com>
2022-03-24 14:48:43 +0700

    get Pokemon API

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
 
-function App() {
+interface Pokemon {
+  name: string;
+  url: string;
+}
+
+const App: React.FC = () => {
+  const [pokemons, setPokemons] = useState<string[]>([]);
+  useEffect(() => {
+    const getPokemons = async () => {
+      const respone = await axios.get(
+        "https://pokeapi.co/api/v2/pokemon?limit=20&offset=20"
+      );
+
+      respone.data.results.forEach(async (pokemon: Pokemon) => {
+        const poke = await axios.get(
+          `https://pokeapi.co/api/v2/pokemon/${pokemon.name}`
+        );
+        console.log(poke.data);
+      });
+      console.log(respone.data);
+    };
+    getPokemons();
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <header className="pokemon-header">Pokemon </header>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
